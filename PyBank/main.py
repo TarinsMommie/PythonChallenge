22634b7a0a33b98da306da8bbb84694026996541
Ms Cass <147094420+TarinsMommie@users.noreply.github.com>
2023-11-16 12:37:17 -0600

    2nd Update by Cass

--- PyBank/main.py
+++ PyBank/main.py
@@ -8,3 +8,10 @@ budget_data = os.path.join("..","pybank","resources","budget_data.csv")
 with open(budget_data) as csvfile:
 #csvreader
 csv_reader = csv.reader(csv_file, delimiter=",")
+#Count rows but not header
+for row in csv_reader:
+    if not header_skipped:
+        header_skipped = True
+        continue 
+    Total_Months += 1
+    print (f'Total Months: {Total_Months}")
