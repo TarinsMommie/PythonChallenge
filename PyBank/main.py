56bd02671701677008f293a74ed23924d464ecd3
Ms Cass <147094420+TarinsMommie@users.noreply.github.com>
2023-11-16 12:55:31 -0600

    3rd update

--- PyBank/main.py
+++ PyBank/main.py
@@ -8,10 +8,18 @@ budget_data = os.path.join("..","pybank","resources","budget_data.csv")
 with open(budget_data) as csvfile:
 #csvreader
 csv_reader = csv.reader(csv_file, delimiter=",")
-#Count rows but not header
-for row in csv_reader:
+#Number of Months:
+column_index = 0
     if not header_skipped:
         header_skipped = True
         continue 
     Total_Months += 1
     print (f'Total Months: {Total_Months}")
+#Average Change:
+column_index = 1
+    if not header_skipped:
+        header_skipped = True
+        continue 
+    Average_Net_Loses += 1
+    print (f'Average Change: {Average_Net_Loses")
+    
